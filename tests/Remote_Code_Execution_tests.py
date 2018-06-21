91da2ecb701c8880b684947dec4ec34fd3d33cd8
jerini <apollloo@interia.pl>
2018-06-22 00:10:44 +0200

    Testy

--- tests/Remote_Code_Execution_tests.py
+++ tests/Remote_Code_Execution_tests.py
@@ -4,31 +4,31 @@ import rpyc
 import os
 import time
 import sys
+#czesc Wojtek S
+ FILE_PATH = 'Received_codes'
+ SERVER_DIRECTORY = os.getcwd() + '\Remote_Code_Execution\Rpyc_slave_service localhost'
+ SERVER_IP_ADDRESS = 'localhost'
+ PORT = 18861
 
-FILE_PATH = 'Received_codes'
-SERVER_DIRECTORY = os.getcwd() + '\Remote_Code_Execution\Rpyc_slave_service localhost'
-SERVER_IP_ADDRESS = 'localhost'
-PORT = 18861
-
-def setup():
+ def setup():
 	os.system('start cmd /c {}'.format(SERVER_DIRECTORY)) #if server won't start, no tests will pass
 	time.sleep(1)
 	print('SETUP!')
 
-def teardown():
+ def teardown():
 	print('TEAR DOWN!')
 	
-def test_bad_code():
+ def test_bad_code():
 	conn = rpyc.connect(SERVER_IP_ADDRESS, PORT)
 	res = conn.root.send_and_check_code('print "hello world!"')
 	assert_equal(res, False)
 	
-def test_good_code():
+ def test_good_code():
 	conn = rpyc.connect(SERVER_IP_ADDRESS, PORT)
 	res = conn.root.send_and_check_code('print(\'hello world!\')')
 	assert_equal(res, True)
 	
-def test_code_return():
+ def test_code_return():
 	code = open("tests/python_code1.txt", 'r')
 	conn = rpyc.connect(SERVER_IP_ADDRESS, PORT)
 	conn.root.send_and_check_code(code.read())
